59a9b22d80abc473895bd69914a915957ced2261
richardpromise <richardpromise013@gmail.com>
2024-05-29 16:30:15 +0100

    update

---
+++ content/js/app.js
@@ -0,0 +1,104 @@
+// function sayHelloTOUser() {
+//   alert("hello user ypu re welshdfasiohfailk");
+// }
+
+// using the console
+// variable declaration
+// data types
+
+// java script is a zero based index language
+// it's loosely typed
+// its also case sensitive
+// 123456789
+// Case
+// case
+
+// variable decleration
+
+let namE = "john";
+let school = "bafuto";
+
+// variable declarator
+// var (es5)
+// let
+// const
+
+console.log(school);
+// var userName = "john maxwell";
+// console.log(userName);
+const lastName = "Doe";
+console.log(typeof lastName);
+
+namE = "tunde";
+namE = "sharon";
+
+// console.log(haveMOney);
+
+// let allows the value of a vaarioble  reasignable
+
+// let nAmE = "jsjdkl"
+
+// data types
+
+// primitive
+// non primitve
+
+// primitive
+// string
+// number
+// boolean
+// undefined
+// null
+// object
+// symbols
+//
+
+// non primitive
+// arrays
+// functions
+// date/time
+// conditionals
+
+// primitive
+// string
+// set characters in a double or single quote
+let val =
+  "sdfjksdjflksjkljsfskl2287278y27839@@@$%^&*(()_____Oiruieuwuwsjmsj3637";
+
+console.log(typeof val);
+
+// number
+let sum = (100 / 10) * 5 - 2 + 20;
+console.log(sum);
+
+// boolean
+
+// is a true or false statement
+
+let married = true;
+
+// undefined is variable with no value
+
+let somethig;
+
+console.log(somethig);
+
+// null non existance or intentions set value
+
+console.log(localStorage.getItem("item"));
+
+let user = null;
+console.log(user);
+
+// object a make up of properties
+
+let onyinyechukwu = {
+  haveLaptop: true,
+  complextion: "very dark",
+  age: 18,
+  status: "flirt",
+  accountBalance: null,
+  wearsGlasses: "yes",
+};
+
+console.log(onyinyechukwu.status);
